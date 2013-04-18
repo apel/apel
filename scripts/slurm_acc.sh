1aef9025b5c55a561b2243034d70dc7166a44b25
willrogers <wahrogers@gmail.com>
2013-04-18 16:36:03 +0100

    Rename scripts directory, and add the slurm_acc.sh script for configuring slurm accounting.

---
+++ scripts/slurm_acc.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+/bin/sleep 2
+
+NOW=$(date +"%Y%m%d")
+FILE="/var/log/apel/slurm_acc.$NOW"
+
+/usr/local/bin/sacct -P -n --format=JobID,JobName,User,Group,Start,End,Elapsed,CPUTimeRAW,Partition,NCPUS,NNodes,NodeList,MaxRSS,MaxVMSize,State -j $JOBID >> $FILE
+
