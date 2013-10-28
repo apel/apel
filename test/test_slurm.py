b4309a3da20e2b749d22aa049ad362eede0481ef
Adrian Coveney <adrian.coveney@stfc.ac.uk>
2013-10-28 16:46:47 +0000

    Add more test strings to test_slurm.

--- test/test_slurm.py
+++ test/test_slurm.py
@@ -24,6 +24,8 @@ class ParserSlurmTest(unittest.TestCase):
     def test_parse_line(self):
 
         line1 = ('1000|cream_176801680|dteam005|dteam|2013-03-27T17:13:24|2013-03-27T17:13:26|00:00:02|2|prod|1|1|cert-40|||COMPLETED')
+        line2 = ('278952.batch|batch|||2013-10-23T21:37:24|2013-10-25T00:01:37|1-02:24:13|95053||1|1|wn36|438.50M|1567524K|COMPLETED')
+        line3 = ('297720.batch|batch|||2013-10-25T12:11:20|2013-10-25T12:11:36|00:00:16|16||1|1|wn37|3228K|23820K|COMPLETED')
 
         line1_values = {"JobName": "1000",
                         "LocalUserID": "dteam005",
@@ -38,7 +40,35 @@ class ParserSlurmTest(unittest.TestCase):
                         "Processors": 1
                         }
 
-        cases = {line1: line1_values}
+        line2_values = {"JobName": "278952.batch",
+                        "LocalUserID": None,
+                        "LocalUserGroup": None,
+                        "WallDuration": 95053,
+                        "CpuDuration": 95053,
+                        "StartTime": datetime(2013, 10, 23, 21, 37, 24),
+                        "StopTime": datetime(2013, 10, 25, 00, 01, 37),
+                        "MemoryReal": 438500,
+                        "MemoryVirtual": 1567524,
+                        "NodeCount": 1,
+                        "Processors": 1
+                        }
+
+        line3_values = {"JobName": "297720.batch",
+                        "LocalUserID": None,
+                        "LocalUserGroup": None,
+                        "WallDuration": 16,
+                        "CpuDuration": 16,
+                        "StartTime": datetime(2013, 10, 25, 12, 11, 20),
+                        "StopTime": datetime(2013, 10, 25, 12, 11, 36),
+                        "MemoryReal": 3228,
+                        "MemoryVirtual": 23820,
+                        "NodeCount": 1,
+                        "Processors": 1
+                        }
+
+        cases = {line1: line1_values,
+                 line2: line2_values,
+                 line3: line3_values}
 
         for line in cases.keys():
             record = self.parser.parse(line)
