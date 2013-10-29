8fa4afb2de024eb22b1e532741b0f05f02ffae6f
Adrian Coveney <adrian.coveney@stfc.ac.uk>
2013-10-29 12:40:24 +0000

    Alter slurm parser to handle memory sizes in MB.
    - Add function to normalise memory size strings to KB.
    - Raise ValueError on being passed an unknown unit prefix.

--- apel/parsers/slurm.py
+++ apel/parsers/slurm.py
@@ -12,7 +12,7 @@
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
    See the License for the specific language governing permissions and
    limitations under the License.
-   
+
    @author: Lisa Zangrando, Will Rogers
 '''
 from apel.common import parse_time
@@ -30,7 +30,7 @@ def parse_local_timestamp(datetime_string):
     '''
     Parse a timestamp without TZ information, assuming that it is
     referring to system time.  Return a datetime converted to UTC.
-    
+
     SLURM timestamps are in the form '2013-06-01T10:00:00'.
     '''
     unix_time = time.mktime(time.strptime(datetime_string, "%Y-%m-%dT%H:%M:%S"))
@@ -44,32 +44,25 @@ class SlurmParser(Parser):
     def __init__(self, site, machine_name, mpi):
         Parser.__init__(self, site, machine_name, mpi)
         log.info('Site: %s; batch system: %s' % (self.site_name, self.machine_name))
-    
+
     def parse(self, line):
         '''
         Parses single line from accounting log file.
         '''
         # /usr/local/bin/sacct -P -n --format=JobID,JobName,User,Group,Start,End,Elapsed,CPUTimeRAW,Partition,NCPUS,NNodes,NodeList,MaxRSS,MaxVMSize,State -j $JOBID >> /var/log/apel/slurm_acc.20130311
         # 1007|cream_612883006|dteam005|dteam|2013-03-27T17:13:41|2013-03-27T17:13:44|00:00:03|3|prod|1|1|cert-40|||COMPLETED
-        
+
         # log.info('line: %s' % (line));
         values = line.strip().split('|')
-        
+
         if values[14] != 'COMPLETED':
             return None
 
-        rmem = None
-        if values[12]:
-            # remove 'K' string from the end
-            rmem = int(values[12][:-1])
+        rmem = self._normalise_memory(values[12])
 
-        vmem = None
-        if values[13]:
-            # remove 'K' string from the end
-            vmem = int(values[13][:-1])
+        vmem = self._normalise_memory(values[13])
 
-        mapping = {
-                   'Site'            : lambda x: self.site_name,
+        mapping = {'Site'            : lambda x: self.site_name,
                    'MachineName'     : lambda x: self.machine_name,
                    'Infrastructure'  : lambda x: "APEL-CREAM-SLURM",
                    'JobName'         : lambda x: x[0],
@@ -81,11 +74,11 @@ class SlurmParser(Parser):
                    'StartTime'       : lambda x: parse_local_timestamp(x[4]),
                    'StopTime'        : lambda x: parse_local_timestamp(x[5]),
                    'Queue'           : lambda x: x[9],
-                   'MemoryReal'      : lambda x: rmem, # KB
-                   'MemoryVirtual'   : lambda x: vmem, # KB
+                   'MemoryReal'      : lambda x: rmem,  # KB
+                   'MemoryVirtual'   : lambda x: vmem,  # KB
                    'Processors'      : lambda x: int(x[9]),
                    'NodeCount'       : lambda x: int(x[10])
-        }
+                   }
 
         rc = {}
 
@@ -97,8 +90,17 @@ class SlurmParser(Parser):
 
         record = EventRecord()
         record.set_all(rc)
-        return record      
+        return record
 
-
-
-    
+    def _normalise_memory(self, mem):
+        """Strip unit prefix and return memory size as int in KB."""
+        if mem:
+            if mem[-1:] == 'K':
+                mem = int(mem[:-1])
+            elif mem[-1:] == 'M':
+                mem = int(float(mem[:-1])*1024)
+            else:
+                raise ValueError("Unsupported unit prefix '%s'. Expected 'K' or 'M'." % mem[-1:])
+        else:
+            mem = None
+        return mem
