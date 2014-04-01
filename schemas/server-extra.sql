4eec85c97ab1c656a12525dec35f59dc2ef0ec29
Adrian Coveney <adrian.coveney@stfc.ac.uk>
2014-04-01 15:01:22 +0100

    Initial commit of extra views for central server.

---
+++ schemas/server-extra.sql
@@ -0,0 +1,115 @@
+-- Extra views on NormalisedSuperSummaries for the central APEL server.
+-- This schema does not need to be loaded by regional servers.
+
+-- TODO Remove factors of 1000 depending on whether SI2K or kSI2K is used
+
+DROP VIEW IF EXISTS VSumCPU;
+
+CREATE VIEW VSumCPU AS
+    SELECT
+        Sites.name AS ExecutingSite,
+        VOs.name AS LCGUserVO,
+        CAST(SUM(NumberOfJobs) AS UNSIGNED) AS Njobs,
+        ROUND((SUM(CpuDuration) / 3600), 0) AS SumCPU,
+        ROUND(SUM((CpuDuration * IF((ServiceLevelType = 'HEPSPEC'),(ServiceLevel * 250),ServiceLevel)) / (1000 * 3600)), 0) AS NormSumCPU,
+        ROUND((SUM(WallDuration) / 3600), 0) AS SumWCT,
+        ROUND(SUM((WallDuration * IF((ServiceLevelType = 'HEPSPEC'),(ServiceLevel * 250),ServiceLevel)) / (1000 * 3600)), 0) AS NormSumWCT,
+        Month,
+        Year,
+        CAST(MIN(EarliestEndTime) AS DATE) AS RecordStart,
+        CAST(MAX(LatestEndTime) AS DATE) AS RecordEnd
+    FROM
+        NormalisedSuperSummaries,
+        Sites,
+        VOs
+    WHERE
+        NumberOfJobs > 0
+        AND SiteID = Sites.id
+        AND VOID = VOs.id
+    GROUP BY
+        Sites.name,
+        VOs.name,
+        Month,
+        Year;
+
+
+DROP VIEW IF EXISTS VUserCPU;
+
+CREATE VIEW VUserCPU AS
+    SELECT
+        Sites.name AS ExecutingSite,
+        VOs.name AS LCGUserVO,
+        DNs.name AS UserDN,
+        VOGroups.name AS PrimaryGroup,
+        VORoles.name AS PrimaryRole,
+        CAST(SUM(NumberOfJobs) AS UNSIGNED) AS Njobs,
+        ROUND((SUM(CpuDuration) / 3600),0) AS SumCPU,
+        ROUND(SUM((CpuDuration * IF((ServiceLevelType = 'HEPSPEC'),(ServiceLevel * 250),ServiceLevel)) / (1000 * 3600)),0) AS NormSumCPU,
+        ROUND((SUM(WallDuration) / 3600),0) AS SumWCT,
+        ROUND(SUM((WallDuration * IF((ServiceLevelType = 'HEPSPEC'),(ServiceLevel * 250),ServiceLevel)) / (1000 * 3600)),0) AS NormSumWCT,
+        Month,
+        Year,
+        CAST(MIN(EarliestEndTime) AS DATE) AS RecordStart,
+        CAST(MAX(LatestEndTime) AS DATE) AS RecordEnd
+    FROM 
+        NormalisedSuperSummaries,
+        Sites,
+        VOs,
+        DNs,
+        VOGroups,
+        VORoles
+    WHERE
+        NumberOfJobs > 0
+        AND SiteID = Sites.id
+        AND VOID = VOs.id
+        AND GlobalUserNameID = DNs.id
+        AND VOGroupID = VOGroups.id
+        AND VORoleID = VORoles.id
+    GROUP BY
+        Sites.name,
+        VOs.name,
+        DNs.name,
+        VOGroups.name,
+        VORoles.name,
+        Month,
+        Year;
+
+
+DROP VIEW IF EXISTS VSpecIntHistory;
+
+CREATE VIEW VSpecIntHistory AS
+    SELECT
+        site.name AS ExecutingSite,
+        CAST(ROUND(IF((ServiceLevelType = 'HEPSPEC'),(ServiceLevel * 250),ServiceLevel),0) AS UNSIGNED) AS SpecInt2000,
+        CAST(SUM(NumberOfJobs) AS UNSIGNED) AS Njobs,
+        Month,
+        Year,
+        CAST(MIN(EarliestEndTime) AS DATE) AS RecordStart,
+        CAST(MAX(LatestEndTime) AS DATE) AS RecordEnd
+    FROM
+        NormalisedSuperSummaries JOIN Sites AS site ON SiteID = site.id
+    GROUP BY
+        SiteID,
+        SpecInt2000,
+        Year,
+        Month;
+
+
+DROP VIEW IF EXISTS HepSpecHistory;
+
+CREATE VIEW VHepSpecHistory AS
+    SELECT
+        site.name AS Site,
+        IF((ServiceLevelType = 'HEPSPEC'),ServiceLevel,(ServiceLevel / 250)) AS HepSpec06,
+        SUM(NumberOfJobs) AS NumberOfJobs,
+        Year,
+        Month,
+        CAST(MIN(EarliestEndTime) AS DATE) AS EarliestEndDate,
+        CAST(MAX(LatestEndTime) AS DATE) AS LatestEndDate
+    FROM
+        NormalisedSuperSummaries JOIN Sites AS site ON SiteID = site.id
+    GROUP BY
+        SiteID,
+        HepSpec06,
+        Year,
+        Month;
