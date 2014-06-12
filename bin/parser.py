e22d7093a033f99e03b39abf00bd7552dec97429
Adrian Coveney <adrian.coveney@stfc.ac.uk>
2014-06-12 09:31:00 +0100

    Revert "Revert add warning when a parse failed previously"
    This reverts commit b43b0c9554129080c5e6693ddd63a223f761dd27.

--- bin/parser.py
+++ bin/parser.py
@@ -166,6 +166,9 @@ def scan_dir(parser, dirpath, reparse, expr, apel_db, processed):
                         # we will leave this record unmodified
                         updated.append(pf)
                         found = True
+                        # Check for zero parsed lines so we can warn later on.
+                        if pf.get_field('Parsed') == 0:
+                            unparsed = True
                         break  # If we find a match, no need to keep checking.
                         
                 if reparse or not found:
@@ -194,6 +197,9 @@ def scan_dir(parser, dirpath, reparse, expr, apel_db, processed):
                         pr.set_field('StopLine', total)
                         pr.set_field('Parsed', parsed)
                         updated.append(pr)
+                elif unparsed:
+                    log.warn('%s already parsed unsuccessfully, skipping'
+                             % abs_file)
                 else:
                     log.info('%s already parsed, skipping' % abs_file)
             elif os.path.isfile(abs_file):
