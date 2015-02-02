7723ce2cd3bd2b64c30f7fe70c1c7e6834042ac4
Adrian Coveney <adrian.coveney@stfc.ac.uk>
2015-02-02 15:45:24 +0000

    Add extra sample line to test_sge.py

--- test/test_sge.py
+++ test/test_sge.py
@@ -72,7 +72,30 @@ class ParserSGETest(unittest.TestCase):
                         }
         
         cases[line3] = line3_values
-        
+
+        line4 = ("grid.q:node101.cm.cluster:prdatl:prdatl26:cream_324080374:329"
+                 "4013:sge:0:1404167579:1404169049:1404169408:0:0:359:32.189106"
+                 ":18.607171:99104.000000:0:0:0:0:1957472:1546:0:483672.000000:"
+                 "20488:0:0:0:151429:47022:NONE:defaultdepartment:NONE:1:0:50.7"
+                 "96277:4.497218:0.339940:-u prdatl26 -q grid.q -binding no_job"
+                 "_binding 0 0 0 0 no_explicit_binding:0.000000:NONE:5944487936"
+                 ".000000:0:0:NONE")
+
+        line4_values = {'JobName': "3294013",
+                        'LocalUserID': "prdatl26",
+                        'LocalUserGroup': "prdatl",
+                        'WallDuration': 359,
+                        'CpuDuration': 51,
+                        'StartTime': datetime.utcfromtimestamp(1404169049),
+                        'StopTime': datetime.utcfromtimestamp(1404169408),
+                        'MemoryReal': int(4.497218*1024*1024),
+                        'MemoryVirtual': 5944487936,
+                        'NodeCount': 0,
+                        'Processors': 1
+                        }
+
+        cases[line4] = line4_values
+
         for line in cases.keys():
             record = self.parser.parse(line)
             cont = record._record_content
