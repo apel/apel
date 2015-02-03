684ff1f65718e114e8a4cbada584c2e7253776c5
Adrian Coveney <adrian.coveney@stfc.ac.uk>
2015-02-03 09:46:46 +0000

    Remove unnecessary comments

--- test/test_parser.py
+++ test/test_parser.py
@@ -15,11 +15,6 @@ class ParserTest(unittest.TestCase):
 
     def setUp(self):
         self.tf = tempfile.TemporaryFile()
-        #print self.tf.name
-        #self.tf.write('Test text.')
-        ## Reset file position to start so it can be read
-        #self.tf.seek(0)
-        #print self.tf.readline()
         self.patcher = mock.patch('apel.db.apeldb.ApelDb')
         self.mock_db = self.patcher.start()
 
