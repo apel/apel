f0794a0f0d3b5e24c59f846c433d6e9d123fb963
Adrian Coveney <adrian.coveney@stfc.ac.uk>
2016-09-08 17:33:17 +0100

    Fix storage record test so disabled test works

--- test/test_storage_record.py
+++ test/test_storage_record.py
@@ -8,34 +8,42 @@ class StorageRecordTest(unittest.TestCase):
     Test case for StorageRecord
     """
 
-#    def test_load_from_tuple(self):
-#        # full example
-#        data = ('host.example.org/sr/87912469269276', 1289293612, 'host.example.org', 'MySite',
-#                 'pool-003', 'disk', 'replicated', 42, '/home/projectA', 
-#                 'johndoe', 'projectA', '/O=Grid/OU=example.org/CN=John Doe',
-#                 'binarydataproject.example.org', '2010-10-11T09:31:40Z', '2010-10-11T09:41:40Z',
-#                 14728, 13617)
-#        
-#        record = StorageRecord()
-#        record.load_from_tuple(data)
-#        
-#        self.assertEquals(record.get_field('RecordId'), 'host.example.org/sr/87912469269276')
-#        self.assertEquals(str(record.get_field('CreateTime')), "2010-11-09 09:06:52")
-#        self.assertEquals(record.get_field('StorageSystem'), 'host.example.org')
-#        self.assertEquals(record.get_field('Site'), 'MySite')
-#        self.assertEquals(record.get_field('StorageShare'), 'pool-003')
-#        self.assertEquals(record.get_field('StorageMedia'), 'disk')
-#        self.assertEquals(record.get_field('StorageClass'), 'replicated')
-#        self.assertEquals(record.get_field('FileCount'), 42)
-#        self.assertEquals(record.get_field('DirectoryPath'), '/home/projectA')
-#        self.assertEquals(record.get_field('LocalUser'), 'johndoe')
-#        self.assertEquals(record.get_field('LocalGroup'), 'projectA')
-#        self.assertEquals(record.get_field('UserIdentity'), '/O=Grid/OU=example.org/CN=John Doe')
-#        self.assertEquals(record.get_field('GroupName'), 'binarydataproject.example.org')
-#        self.assertEquals(str(record.get_field('StartTime')), "2010-10-11 09:31:40")
-#        self.assertEquals(str(record.get_field('EndTime')), "2010-10-11 09:41:40")
-#        self.assertEquals(record.get_field('ResourceCapacityUsed'), 14728)
-#        self.assertEquals(record.get_field('LogicalCapacityUsed'), 13617)
+    def test_load_from_tuple(self):
+        # full example
+        data = ('host.example.org/sr/87912469269276', 1289293612,
+                'host.example.org', 'MySite', 'pool-003', 'disk', 'replicated',
+                42, '/home/projectA', 'johndoe', 'projectA',
+                '/O=Grid/OU=example.org/CN=John Doe',
+                'binarydataproject.example.org', '2010-10-11T09:31:40Z',
+                '2010-10-11T09:41:40Z', 14728, 13617, 0)
+
+        record = StorageRecord()
+        record.load_from_tuple(data)
+
+        self.assertEquals(record.get_field('RecordId'),
+                          'host.example.org/sr/87912469269276')
+        self.assertEquals(str(record.get_field('CreateTime')),
+                          "2010-11-09 09:06:52")
+        self.assertEquals(record.get_field('StorageSystem'), 'host.example.org')
+        self.assertEquals(record.get_field('Site'), 'MySite')
+        self.assertEquals(record.get_field('StorageShare'), 'pool-003')
+        self.assertEquals(record.get_field('StorageMedia'), 'disk')
+        self.assertEquals(record.get_field('StorageClass'), 'replicated')
+        self.assertEquals(record.get_field('FileCount'), 42)
+        self.assertEquals(record.get_field('DirectoryPath'), '/home/projectA')
+        self.assertEquals(record.get_field('LocalUser'), 'johndoe')
+        self.assertEquals(record.get_field('LocalGroup'), 'projectA')
+        self.assertEquals(record.get_field('UserIdentity'),
+                          '/O=Grid/OU=example.org/CN=John Doe')
+        self.assertEquals(record.get_field('Group'),
+                          'binarydataproject.example.org')
+        self.assertEquals(str(record.get_field('StartTime')),
+                          "2010-10-11 09:31:40")
+        self.assertEquals(str(record.get_field('EndTime')),
+                          "2010-10-11 09:41:40")
+        self.assertEquals(record.get_field('ResourceCapacityUsed'), 14728)
+        self.assertEquals(record.get_field('LogicalCapacityUsed'), 13617)
+        self.assertEquals(record.get_field('ResourceCapacityAllocated'), 0)
 
     def test_mandatory_fields(self):
         record = StorageRecord()
