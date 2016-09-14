dcadb27a545add41fabf051b6cc6bc078f7bc81b
Adrian Coveney <adrian.coveney@stfc.ac.uk>
2016-09-14 16:10:30 +0100

    Update StorageRecord procedure call template
    - Add two fields to the StorageRecord replace procedure call template to
      support the two extra columns that the table now has.

--- apel/db/backends/mysql.py
+++ apel/db/backends/mysql.py
@@ -70,7 +70,7 @@ class ApelMysqlDb(object):
               ProcessedRecord : "CALL ReplaceProcessedFile(%s, %s, %s, %s, %s)",
               CloudRecord : "CALL ReplaceCloudRecord(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
               CloudSummaryRecord : "CALL ReplaceCloudSummaryRecord(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
-              StorageRecord: "CALL ReplaceStarRecord(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
+              StorageRecord: "CALL ReplaceStarRecord(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
               GroupAttributeRecord: "CALL ReplaceGroupAttribute(%s, %s, %s)"
               }
     
