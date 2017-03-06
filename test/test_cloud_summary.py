992a8b892767c0be66d2dd1078e5010afe4f45d7
Adrian Coveney <adrian.coveney@stfc.ac.uk>
2017-03-06 15:12:47 +0000

    Tidy up cloud summary record test
    - Make test runnable as a script for each of local use.
    - Tidy up whitespace.
    - Rename test class to reflect the fact that it's the summaries being
      tested not just the individual records.

--- test/test_cloud_summary.py
+++ test/test_cloud_summary.py
@@ -3,11 +3,11 @@ import unittest
 from apel.db.records import CloudSummaryRecord
 
 
-class CloudRecordTest(unittest.TestCase):
+class CloudSummaryRecordTest(unittest.TestCase):
     '''
     Test case for CloudSummaryRecord
     '''
-    
+
     def setUp(self):
         self._msg1 = '''
 SiteName: CESNET
@@ -33,9 +33,9 @@ BenchmarkType: Si2k
 Benchmark: 1006.3
 NumberOfVMs: 1
 '''
-        
+
         self._values1 = {'SiteName': 'CESNET',
-                        'CloudComputeService': 'OpenNebula Service A', 
+                        'CloudComputeService': 'OpenNebula Service A',
                         'Status': 'completed',
                         'CloudType': 'OpenNebula',
                         'NetworkInbound': 0,
@@ -46,24 +46,22 @@ NumberOfVMs: 1
                         'Benchmark': 1006.3,
                         'NumberOfVMs': 1
                         }
-        
-        
+
         self.cases = {}
         self.cases[self._msg1] = self._values1
 
     def test_load_from_msg(self):
-        
+
         for msg in self.cases.keys():
-        
+
             cr = CloudSummaryRecord()
             cr.load_from_msg(msg)
-            
+
             cont = cr._record_content
-        
+
             for key in self.cases[msg].keys():
                 self.assertEqual(cont[key], self.cases[msg][key], "%s != %s for key %s" % (cont[key], self.cases[msg][key], key))
-        
-        
+
     def test_mandatory_fields(self):
         record = CloudSummaryRecord()
         record.set_field('SiteName', 'MySite')
@@ -77,11 +75,11 @@ NumberOfVMs: 1
         record.set_field('CloudType', 'OpenStack')
         record.set_field('ImageId', 'img-01')
         record.set_field('NumberOfVMs', 1)
-        
+
         try:
             record._check_fields()
         except Exception, e:
-            self.fail('_check_fields method failed: %s [%s]' % (str(e), str(type(e))))
+            self.fail('_check_fields method failed: %s [%s]' % (e, type(e)))
 
 if __name__ == '__main__':
     unittest.main()
