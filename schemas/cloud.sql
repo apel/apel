97ecccf61bb63ec346cb1eacfa47ae8421272cf5
Greg Corbett <greg.corbett@stfc.ac.uk>
2017-03-14 16:02:18 +0000

    Add protoype v2 LRVM support - ReplaceCloudRecord
    - a MeasurementTime has been added, to represent when the
      record was created. From this, MeasurementMonth and
      MeasurementYear have been added.
    - MeasurementMonth and MeasurementYear added to primary key,
      so that at load time we can ensure only one record (per vm)
      is stored, which will hopefully speed up the LRVM summariser
    - Alter ReplaceCloudRecord to replace old record with an incoming
      only if the MeasurementTime has increased. So that if messages
      are recieved out of order we keep the newer one (the one with the
      greatest MeasurementTime)
    - There doesn't seem to be a nicer way to do the 'ON DUPLICATE KEY UPDATE'
      other than field by field
    - And we can't get around making the greater than comparison everytime
      as we can only reference the current value in the
      'ON DUPLICATE KEY UPDATE' block

--- schemas/cloud.sql
+++ schemas/cloud.sql
@@ -23,8 +23,11 @@ CREATE TABLE CloudRecords (
 
   StartTime DATETIME, 
   EndTime DATETIME, 
-  SuspendDuration INT, 
+  MeasurementTime DATETIME NOT NULL,
+  MeasurementMonth INT NOT NULL,
+  MeasurementYear INT NOT NULL,
 
+  SuspendDuration INT, 
   WallDuration INT, 
   CpuDuration INT, 
   CpuCount INT, 
@@ -45,7 +48,9 @@ CREATE TABLE CloudRecords (
 
   PublisherDNID INT NOT NULL,	    -- Foreign key
 
-  PRIMARY KEY (VMUUID),
+  -- Measurement(Month, Year) allows for usage to be assigned
+  -- to accounting periods at load time
+  PRIMARY KEY (VMUUID, MeasurementMonth, MeasurementYear),
 
   INDEX (UpdateTime),
   INDEX (GlobalUserNameID),
@@ -72,18 +77,93 @@ CREATE PROCEDURE ReplaceCloudRecord(
   disk INT, benchmarkType VARCHAR(50), benchmark DECIMAL(10,3), storageRecordId VARCHAR(255),
   imageId VARCHAR(255), cloudType VARCHAR(255),
   publisherDN VARCHAR(255))
+
 BEGIN
-    REPLACE INTO CloudRecords(VMUUID, SiteID, CloudComputeServiceID, MachineName, LocalUserId, LocalGroupId,
-        GlobalUserNameID, FQAN, VOID, VOGroupID, VORoleID, Status, StartTime, EndTime, SuspendDuration,
-        WallDuration, CpuDuration, CpuCount, NetworkType, NetworkInbound, NetworkOutbound, PublicIPCount, Memory, Disk, BenchmarkType, Benchmark, 
-        StorageRecordId, ImageId, CloudType, PublisherDNID)
+    DECLARE suspendDurationNotNull INT;
+    DECLARE cpuDurationNotNull INT;
+    DECLARE wallDurationNotNull INT;
+    DECLARE measurementTimeCalculated DATETIME;
+    
+    -- If the incoming suspendDuration is NULL, we can't compute a MeasurementTime
+    -- so set a resonable value in that case
+    -- DECLARE suspendDurationNotNull INT;
+    SET suspendDurationNotNull = IFNULL(suspendDuration, 0);
+
+    -- If the incoming cpuDuration is NULL, we may not be able to compute a MeasurementTime
+    -- as cpuDuration is used to calculate MeasurementTime if the incoming wallDuration is also NULL
+    -- so set a resonable value so we can use that if needed
+    SET cpuDurationNotNull = IFNULL(cpuDuration, 0);
+
+
+    -- If the incoming wallDuartion is NULL we can't compute a MeasurementTime.
+    -- Based off LRVMv1, we use cpuDurationNotNull, which from above is guaranteed
+    -- to be a resonable value
+    SET wallDurationNotNull = IFNULL(wallDuration,cpuDurationNotNull);
+
+    -- Calculated the time of measurement so we can use it later to determine which
+    -- accounting period this incoming record belongs too.
+    SET measurementTimeCalculated = TIMESTAMPADD(SECOND, (suspendDurationNotNull + wallDurationNotNull), StartTime);
+
+    INSERT INTO CloudRecords(VMUUID, SiteID, CloudComputeServiceID, MachineName,
+        LocalUserId, LocalGroupId, GlobalUserNameID, FQAN, VOID, VOGroupID,
+        VORoleID, Status, StartTime, EndTime, MeasurementTime, MeasurementMonth,
+        MeasurementYear, SuspendDuration, WallDuration, CpuDuration, CpuCount,
+        NetworkType, NetworkInbound, NetworkOutbound, PublicIPCount, Memory, Disk,
+        BenchmarkType, Benchmark, StorageRecordId, ImageId, CloudType, PublisherDNID)
       VALUES (
-        VMUUID, SiteLookup(site), CloudComputeServiceLookup(cloudComputeService), machineName, localUserId, localGroupId, DNLookup(globalUserName), 
-        fqan, VOLookup(vo),
-        VOGroupLookup(voGroup), VORoleLookup(voRole), status, startTime, endTime, suspendDuration, 
-        wallDuration, cpuDuration, cpuCount, networkType, networkInbound, networkOutbound, publicIPCount, memory,
-        disk, benchmarkType, benchmark, storageRecordId, imageId, cloudType, DNLookup(publisherDN)
-        );
+        VMUUID, SiteLookup(site), CloudComputeServiceLookup(cloudComputeService), machineName,
+        localUserId, localGroupId, DNLookup(globalUserName), fqan, VOLookup(vo), VOGroupLookup(voGroup),
+        VORoleLookup(voRole), status, startTime, endTime, measurementTimeCalculated, Month(measurementTimeCalculated), Year(measurementTimeCalculated),
+        suspendDurationNotNull, wallDurationNotNull, cpuDurationNotNull, cpuCount,
+        networkType, networkInbound, networkOutbound, publicIPCount, memory, disk,
+        benchmarkType, benchmark, storageRecordId, imageId, cloudType, DNLookup(publisherDN))
+      ON DUPLICATE KEY UPDATE
+        -- Then the incoming record belong in a accounting period which we
+        -- already have some data (for this VM/Site)
+        -- If the incoming measurementTime is greater than the currently stored one
+        -- update all columns with the new values.
+        -- It's possible these updates do not occur in an "all or nothing" fashion
+        -- as per https://thewebfellas.com/blog/conditional-duplicate-key-updates-with-mysql
+        -- so measurementTime is the last thing to be updated.
+        -- There is no nicer way to do the 'ON DUPLICATE KEY UPDATE'
+        -- other than field by field, and we can't get around making the greater than
+        -- comparison everytime as we can only reference the current value in the
+        -- 'ON DUPLICATE KEY UPDATE' block
+        CloudRecords.SiteID = IF(measurementTimeCalculated > CloudRecords.MeasurementTime, SiteLookup(site), CloudRecords.SiteID),
+        CloudRecords.CloudComputeServiceID = IF(measurementTimeCalculated > CloudRecords.MeasurementTime, CloudComputeServiceLookup(cloudComputeService), CloudRecords.CloudComputeServiceID),
+        CloudRecords.MachineName = IF(measurementTimeCalculated > CloudRecords.MeasurementTime, machineName, CloudRecords.MachineName),
+        CloudRecords.LocalUserId = IF(measurementTimeCalculated > CloudRecords.MeasurementTime, localUserId, CloudRecords.LocalUserId),
+        CloudRecords.LocalGroupId = IF(measurementTimeCalculated > CloudRecords.MeasurementTime, localGroupId, CloudRecords.LocalGroupId),
+        CloudRecords.GlobalUserNameID = IF(measurementTimeCalculated > CloudRecords.MeasurementTime, DNLookup(globalUserName), CloudRecords.GlobalUserNameID),
+        CloudRecords.FQAN = IF(measurementTimeCalculated > CloudRecords.MeasurementTime, fqan, CloudRecords.FQAN),
+        CloudRecords.VOID = IF(measurementTimeCalculated > CloudRecords.MeasurementTime, VOLookup(vo), CloudRecords.VOID),
+        CloudRecords.VOGroupID = IF(measurementTimeCalculated > CloudRecords.MeasurementTime, VOGroupLookup(voGroup), CloudRecords.VOGroupID),
+        CloudRecords.VORoleID = IF(measurementTimeCalculated > CloudRecords.MeasurementTime, VORoleLookup(voRole), CloudRecords.VORoleID),
+        CloudRecords.Status = IF(measurementTimeCalculated > CloudRecords.MeasurementTime, status, CloudRecords.Status),
+        CloudRecords.StartTime = IF(measurementTimeCalculated > CloudRecords.MeasurementTime, startTime, CloudRecords.StartTime),
+        CloudRecords.EndTime = IF(measurementTimeCalculated > CloudRecords.MeasurementTime, endTime, CloudRecords.EndTime),
+        CloudRecords.SuspendDuration = IF(measurementTimeCalculated > CloudRecords.MeasurementTime, suspendDurationNotNull, CloudRecords.SuspendDuration),
+        CloudRecords.WallDuration = IF(measurementTimeCalculated > CloudRecords.MeasurementTime, wallDurationNotNull, CloudRecords.WallDuration),
+        CloudRecords.CpuDuration = IF(measurementTimeCalculated > CloudRecords.MeasurementTime, cpuDurationNotNull, CloudRecords.CpuDuration),
+        CloudRecords.CpuCount = IF(measurementTimeCalculated > CloudRecords.MeasurementTime, cpuCount, CloudRecords.CpuCount),
+        CloudRecords.NetworkType = IF(measurementTimeCalculated > CloudRecords.MeasurementTime, networkType, CloudRecords.NetworkType),
+        CloudRecords.NetworkInbound = IF(measurementTimeCalculated > CloudRecords.MeasurementTime, networkInbound, CloudRecords.NetworkInbound),
+        CloudRecords.NetworkOutbound = IF(measurementTimeCalculated > CloudRecords.MeasurementTime, networkOutbound, CloudRecords.NetworkOutbound),
+        CloudRecords.PublicIPCount = IF(measurementTimeCalculated > CloudRecords.MeasurementTime, publicIPCount, CloudRecords.PublicIPCount),
+        CloudRecords.Memory = IF(measurementTimeCalculated > CloudRecords.MeasurementTime, memory, CloudRecords.Memory),
+        CloudRecords.Disk = IF(measurementTimeCalculated > CloudRecords.MeasurementTime, disk, CloudRecords.Disk),
+        CloudRecords.BenchmarkType = IF(measurementTimeCalculated > CloudRecords.MeasurementTime, benchmarkType, CloudRecords.BenchmarkType),
+        CloudRecords.Benchmark = IF(measurementTimeCalculated > CloudRecords.MeasurementTime, Benchmark, CloudRecords.Benchmark),
+        CloudRecords.StorageRecordId = IF(measurementTimeCalculated > CloudRecords.MeasurementTime, storageRecordId, CloudRecords.StorageRecordId),
+        CloudRecords.ImageId = IF(measurementTimeCalculated > CloudRecords.MeasurementTime, imageId, CloudRecords.ImageId),
+        CloudRecords.CloudType = IF(measurementTimeCalculated > CloudRecords.MeasurementTime, cloudType, CloudRecords.CloudType),
+        CloudRecords.PublisherDNID = IF(measurementTimeCalculated > CloudRecords.MeasurementTime, DNLookup(publisherDN), CloudRecords.PublisherDNID),
+
+        CloudRecords.MeasurementMonth = IF(measurementTimeCalculated > CloudRecords.MeasurementTime, Month(measurementTimeCalculated), CloudRecords.MeasurementMonth),
+        CloudRecords.MeasurementYear = IF(measurementTimeCalculated > CloudRecords.MeasurementTime, Year(measurementTimeCalculated), CloudRecords.MeasurementYear),
+
+        CloudRecords.MeasurementTime = IF(measurementTimeCalculated > CloudRecords.MeasurementTime, measurementTimeCalculated, CloudRecords.MeasurementTime)
+    ;
 END //
 DELIMITER ;
 
