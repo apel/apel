36e34be2e4761927218abdbf004082f11732cf3f
Greg Corbett <greg.corbett@stfc.ac.uk>
2017-03-16 17:13:12 +0000

    Add 'static' files for APEL Server Docker image
    - Any instance of the APEL Server needs these files to do
      cloud accounting and do not require start time configuration.
      As such they can be built into the docker image, this requries
      they be in the git repo.
    - cron jobs to run the summariser are expected to change depending
      on deployment so need to be mounted into /etc/cron.d at start up

---
+++ scripts/run_cloud_summariser.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Stop the dbloader
+/sbin/service apeldbloader-cloud stop
+
+# Wait to ensure dbloader has stopped
+sleep 5
+
+# Run the summariser
+python /usr/bin/apelsummariser -d /etc/apel/clouddb.cfg -c /etc/apel/cloudsummariser.cfg
+
+# Wait to ensure summariser has stopped
+sleep 5
+
+# Start the dbloader
+/sbin/service apeldbloader-cloud start
+
