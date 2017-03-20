8deaacf37d316c981ab728fee5e20a1e1dadfccc
Greg Corbett <greg.corbett@stfc.ac.uk>
2017-03-20 15:33:08 +0000

    Use '0000-00-00 00:00:00' to replace NULL values
    - instead of CURRENT_TIMESTAMP as this could cause issues
      determining how recent a record is.

--- scripts/update_timestamp_explicit_cloud.sql
+++ scripts/update_timestamp_explicit_cloud.sql
@@ -1,13 +1,14 @@
 -- UPDATE SCRIPT FOR CLOUD SCHEMA
--- This script will set any null Cloud UpdateTimes to now
+-- This script will set any null Cloud UpdateTimes to the zero timestamp
+-- (to prevent issues determining how recent a record is)
 -- and then explicitly set the UpdateTimes of 
 -- future rows to update
 
-UPDATE CloudRecords SET UpdateTime = CURRENT_TIMESTAMP WHERE UpdateTime IS NULL;
+UPDATE CloudRecords SET UpdateTime = '0000-00-00 00:00:00' WHERE UpdateTime IS NULL;
 ALTER TABLE CloudRecords MODIFY COLUMN UpdateTime TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP;
 
-UPDATE CloudSummaries SET UpdateTime = CURRENT_TIMESTAMP WHERE UpdateTime IS NULL;
+UPDATE CloudSummaries SET UpdateTime = '0000-00-00 00:00:00' WHERE UpdateTime IS NULL;
 ALTER TABLE CloudSummaries MODIFY COLUMN UpdateTime TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP;
 
-UPDATE LastUpdated SET UpdateTime = CURRENT_TIMESTAMP WHERE UpdateTime IS NULL;
+UPDATE LastUpdated SET UpdateTime = '0000-00-00 00:00:00' WHERE UpdateTime IS NULL;
 ALTER TABLE LastUpdated MODIFY COLUMN UpdateTime TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP;
