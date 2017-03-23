8688c12929f1025fcaaaf99acb428d2237405534
Greg Corbett <greg.corbett@stfc.ac.uk>
2017-03-23 17:17:50 +0000

    Make the docker image start the loader first
    - previously, the worst case would be that cron is started
      just before the summariser is started and switches off the loader
      (which has yet to be switched on). Then, this script starts the
      loader while the summariser is running! Which could cause it to run slowly
      or produce inconsistent data
    - this change alleviates this, as now in the above case the cron will always
      prevent the loader form loading

--- docker/run_on_entry.sh
+++ docker/run_on_entry.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 # This file is run as process 1 by instances of the docker container
 # it starts:
-# - crond, to allow the periodic running of the summariser
 # - the cloud loader, as we want to start loadig as soon as the
 #   container is up (rather than waiting for the first summariser run)
+# - crond, to allow the periodic running of the summariser
 #
 # It then goes into an infinite loop as docker entrypoints have to
 # not terminate to keep the container alive
 
-# start cron
-service crond start
-
 # start the loader service
 service apeldbloader-cloud start
+
+# start cron
+service crond start
 
 #keep docker running
 while true
