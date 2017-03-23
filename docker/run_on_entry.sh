097aa3c1535a3f7fe8714311107b82882cf481ac
Greg Corbett <greg.corbett@stfc.ac.uk>
2017-03-23 17:16:24 +0000

    Add explanation of what run_on_entry.sh does

--- docker/run_on_entry.sh
+++ docker/run_on_entry.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+# This file is run as process 1 by instances of the docker container
+# it starts:
+# - crond, to allow the periodic running of the summariser
+# - the cloud loader, as we want to start loadig as soon as the
+#   container is up (rather than waiting for the first summariser run)
+#
+# It then goes into an infinite loop as docker entrypoints have to
+# not terminate to keep the container alive
 
 # start cron
 service crond start
