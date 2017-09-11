572b4052049e7d71fcfa108f192cc660ddd78ecf
Adrian Coveney <adrian.coveney@stfc.ac.uk>
2017-09-11 17:15:35 +0100

    Move tearDown and add docstrings to unloader test

--- test/test_unloader.py
+++ test/test_unloader.py
@@ -18,19 +18,20 @@ class TestDbUnloader(unittest.TestCase):
         self.dir_path = tempfile.mkdtemp()
         self.unloader = DbUnloader(None, self.dir_path)
 
+    def tearDown(self):
+        shutil.rmtree(self.dir_path)
+
     def test_write_star_to_apel(self):
         """Check that unloading StAR records to APEL format is prevented."""
         self.assertRaises(ApelDbException, self.unloader._write_messages,
                           StorageRecord, 'table', 'query', ur=False)
 
-    def tearDown(self):
-        shutil.rmtree(self.dir_path)
-
 
 class TestFunctions(unittest.TestCase):
+    """Test cases for non-class functions."""
 
     def test_get_start_of_previous_month(self):
-
+        """Check that get_start_of_previous_month gives the correct datetime."""
         dt = datetime.datetime(2012, 1, 1, 1, 1, 1)
         dt2 = datetime.datetime(2011, 12, 1, 0, 0, 0)
         ndt = get_start_of_previous_month(dt)
