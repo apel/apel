6e93eab3db7c491f67b6483397399755d37e1759
Adrian Coveney <adrian.coveney@stfc.ac.uk>
2018-08-15 08:56:16 +0100

    Change sacct call to remove path
    This should be safe as this script takes no arguments and is run by
    cron.

--- scripts/slurm_acc.sh
+++ scripts/slurm_acc.sh
@@ -5,4 +5,4 @@
 NOW=$(date +"%Y%m%d")
 FILE="/var/log/apel/slurm_acc.$NOW"
 
-/usr/local/bin/sacct -P -n --format=JobID,JobName,User,Group,Start,End,Elapsed,CPUTimeRAW,Partition,NCPUS,NNodes,NodeList,MaxRSS,MaxVMSize,State -j "$JOBID" >> "$FILE"
+sacct -P -n --format=JobID,JobName,User,Group,Start,End,Elapsed,CPUTimeRAW,Partition,NCPUS,NNodes,NodeList,MaxRSS,MaxVMSize,State -j "$JOBID" >> "$FILE"
