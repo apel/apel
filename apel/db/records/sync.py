8ea506e1463720a0c5d4ef8a34cbd775346a1911
James Adams <james.adams@stfc.ac.uk>
2019-08-01 11:23:57 +0100

    Remove trailing whitespace from apel/db/records/sync.py

--- apel/db/records/sync.py
+++ apel/db/records/sync.py
@@ -12,7 +12,7 @@
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
    See the License for the specific language governing permissions and
    limitations under the License.
-   
+
 @author Will Rogers
 '''
 
@@ -21,28 +21,28 @@ from apel.db.records import Record
 class SyncRecord(Record):
     '''
     Class to represent one sync record.
-    
+
     It knows about the structure of the MySQL table and the message format.
-    It stores its information in a dictionary self._record_content.  The keys 
+    It stores its information in a dictionary self._record_content.  The keys
     are in the same format as in the messages, and are case-sensitive.
     '''
-    
+
     def __init__(self):
         '''Populate fields required to load the message.'''
-        
+
         Record.__init__(self)
-        
+
         # Fields which are required by the message format.
         self._mandatory_fields = ["Site", "NumberOfJobs", "Month", "Year"]
-        
-        # This list allows us to specify the order of lines when we construct 
+
+        # This list allows us to specify the order of lines when we construct
         # records.
         self._msg_fields = ['Site', 'SubmitHost', 'NumberOfJobs', 'Month', 'Year']
 
         # Fields which will have an integer stored in them
         self._int_fields = ["NumberOfJobs", "Month", "Year"]
-        
-        # This list specifies the information that goes in the database.                        
+
+        # This list specifies the information that goes in the database.
         self._db_fields = self._msg_fields
         # All allowed fields.
         self._all_fields = self._msg_fields
