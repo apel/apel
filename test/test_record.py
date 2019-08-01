b1b3651319f5c43d65a9f12dd7b84f88bbe88790
James Adams <james.adams@stfc.ac.uk>
2019-08-01 11:23:57 +0100

    Remove trailing whitespace from test/test_record.py

--- test/test_record.py
+++ test/test_record.py
@@ -12,29 +12,29 @@ class RecordTest(unittest.TestCase):
         record = Record()
         self.assertRaises(InvalidRecordException,
                           record.set_field, 'Test', 'value')
-        
+
         record._db_fields = ['Test']
         record.set_field('Test', 'value')
-        
+
         self.assertEqual(record._record_content['Test'], 'value')
-    
-    
+
+
     def test_set_all(self):
         record = Record()
-        
+
         self.assertRaises(InvalidRecordException,
                           record.set_all, {'Test':'value'})
-        
+
         record._db_fields = ['Test']
         record.set_all({'Test':'value'})
         self.assertEqual(record._record_content['Test'], 'value')
-        
-    
+
+
     def test_get_field(self):
         record = Record()
         record._db_fields = ['Test']
         record._record_content['Test'] = 'value'
-        
+
         self.assertEqual(record.get_field('Test'), 'value')
 
 if __name__ == '__main__':
