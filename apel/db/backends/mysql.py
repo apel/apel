7eaf3f2b48f7875ed90d3d86f6165d9ce45ab37d
James Adams <james.adams@stfc.ac.uk>
2019-08-01 11:26:41 +0100

    Remove trailing whitespace from apel/db/backends/mysql.py

--- apel/db/backends/mysql.py
+++ apel/db/backends/mysql.py
@@ -12,7 +12,7 @@
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
    See the License for the specific language governing permissions and
    limitations under the License.
-   
+
 Created on 27 Oct 2011
 
     @author: Will Rogers, Konrad Jopek
@@ -36,7 +36,7 @@ import datetime
 import logging
 import decimal
 
-# set up the logger 
+# set up the logger
 log = logging.getLogger(__name__)
 # treat MySQL warnings as exceptions
 # warnings.simplefilter("error", category=MySQLdb.Warning)
@@ -55,13 +55,13 @@ class ApelMysqlDb(object):
                     ProcessedRecord : 'VProcessedFiles',
                     SummaryRecord : 'VSummaries',
                     StorageRecord: 'VStarRecords'}
-    
+
     # These simply need to have the same number of arguments as the stored procedures defined in the database schemas.
     INSERT_PROCEDURES = {
               EventRecord : 'CALL InsertEventRecord(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)',
               BlahdRecord : "CALL InsertBlahdRecord(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
               }
-    
+
     REPLACE_PROCEDURES = {
               EventRecord : 'CALL ReplaceEventRecord(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)',
               JobRecord   : "CALL ReplaceJobRecord(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
@@ -74,7 +74,7 @@ class ApelMysqlDb(object):
               StorageRecord: "CALL ReplaceStarRecord(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
               GroupAttributeRecord: "CALL ReplaceGroupAttribute(%s, %s, %s)"
               }
-    
+
     def __init__(self, host, port, username, pwd, db):
         '''
         Initialise variables, and define stored procedures.
@@ -84,7 +84,7 @@ class ApelMysqlDb(object):
         self._db_username = username
         self._db_pwd = pwd
         self._db_name = db
-        
+
         self._summarise_jobs_proc = "SummariseJobs"
         self._normalise_summaries_proc = "NormaliseSummaries"
         self._summarise_vms_proc = "SummariseVMs"
@@ -94,27 +94,27 @@ class ApelMysqlDb(object):
         self._local_jobs_proc = "LocalJobs"
         self._spec_lookup_proc = "SpecLookup (%s, %s, %s, %s)"
         self._spec_update_proc = "CALL SpecUpdate (%s, %s, %s, %s, %s)"
-        
+
         self._processed_clean = "CALL CleanProcessedFiles(%s)"
-        
+
         try:
-            self.db = MySQLdb.connect(host=self._db_host, port=self._db_port, 
-                                      user=self._db_username, passwd=self._db_pwd, 
+            self.db = MySQLdb.connect(host=self._db_host, port=self._db_port,
+                                      user=self._db_username, passwd=self._db_pwd,
                                       db=self._db_name)
         except MySQLdb.Error, e:
             log.error('Error connecting to database: %s', e)
             raise ApelDbException(e)
-        
+
     def test_connection(self):
         '''
         Tests the DB connection - if it fails a MySQLdb.OperationalError will be
         thrown.
         '''
         try:
-            db =  MySQLdb.connect(host=self._db_host, port=self._db_port, 
-                                  user=self._db_username, passwd=self._db_pwd, 
+            db =  MySQLdb.connect(host=self._db_host, port=self._db_port,
+                                  user=self._db_username, passwd=self._db_pwd,
                                   db=self._db_name)
-        
+
             log.info('Connected to ' + self._db_host + ':' + str(self._db_port))
             log.info('Database: ' + self._db_name + '; username: ' + self._db_username)
             db.close()
@@ -148,14 +148,14 @@ class ApelMysqlDb(object):
         except IndexError:
             # no records to load
             return
-            
+
         try:
             # prevent MySQLdb from raising
             # 'MySQL server has gone' exception
             self._mysql_reconnect()
-            
+
             c = self.db.cursor(cursorclass=MySQLdb.cursors.DictCursor)
-            
+
             for record in record_list:
                 values = record.get_db_tuple(source)
                 log.debug('Values: %s', values)
@@ -170,7 +170,7 @@ class ApelMysqlDb(object):
             log.error("Transaction will be rolled back.")
             self.db.rollback()
             raise ApelDbException(err)
-        
+
     def get_records(self, record_type, table_name=None, query=None, rec_number=1000):
         '''
         Yields lists of records fetched from database of the given type.  This is used
@@ -178,14 +178,14 @@ class ApelMysqlDb(object):
         '''
         if table_name is None:
             table_name = self.MYSQL_TABLES[record_type]
-      
+
         select_query = 'SELECT * FROM %s' % (table_name)
 
         if query is not None:
-            select_query += query.get_where() 
-            
+            select_query += query.get_where()
+
         log.debug(select_query)
-        
+
         for batch in self._get_records(record_type, select_query, rec_number):
             yield batch
 
@@ -216,7 +216,7 @@ class ApelMysqlDb(object):
             # prevent MySQLdb from raising
             # 'MySQL server has gone' exception
             self._mysql_reconnect()
-            
+
             c = self.db.cursor(cursorclass=MySQLdb.cursors.SSDictCursor)
             c.execute(query_string)
             while True:
@@ -225,14 +225,14 @@ class ApelMysqlDb(object):
                     # row is a dictionary {Field: Value}
                     record.set_all(row)
                     record_list.append(record)
-                
+
                 if len(record_list) > 0:
                     yield record_list
                     record_list = []
-                
+
                 else:
                     break
-                
+
         except MySQLdb.Error, err:
             log.error('Error during getting records: %s', err)
             log.error('Transaction will be rolled back.')
@@ -249,12 +249,12 @@ class ApelMysqlDb(object):
         c = self.db.cursor()
         c.execute(query)
         result = c.fetchone()
-        
+
         if result is None:
             return None
         else:
             return result[0]
-        
+
     def set_updated(self):
         '''
         Set the current time as the last time messages were sent.
@@ -266,22 +266,22 @@ class ApelMysqlDb(object):
         c.fetchone()
         self.db.commit()
         return True
-    
+
     def check_duplicate_sites(self):
         '''
-        Check that records from the same site are not in both the 
+        Check that records from the same site are not in both the
         JobRecords table and the Summaries table.
         '''
         try:
             # prevent MySQLdb from raising
             # 'MySQL server has gone' exception
             self._mysql_reconnect()
-            
+
             c = self.db.cursor()
-            
+
             c.execute("select count(*) from JobRecords j inner join Summaries s using (SiteID)")
             conflict = c.fetchone()[0] > 0
-            
+
             if conflict:
                 raise ApelDbException("Records exist in both job and summary tables for the same site.")
             self.db.commit()
@@ -292,21 +292,21 @@ class ApelMysqlDb(object):
             if self.db is not None:
                 self.db.rollback()
             raise
-            
+
     def summarise_jobs(self):
         '''
-        Aggregate the data from the JobRecords table and put the results in the 
-        HybridSuperSummaries table.  This method does this by calling the 
+        Aggregate the data from the JobRecords table and put the results in the
+        HybridSuperSummaries table.  This method does this by calling the
         SummariseJobs stored procedure.
-        
-        Any failure will result in the entire transaction being rolled 
+
+        Any failure will result in the entire transaction being rolled
         back.
         '''
         try:
             # prevent MySQLdb from raising
             # 'MySQL server has gone' exception
             self._mysql_reconnect()
-            
+
             c = self.db.cursor()
 
             log.info("Summarising job records...")
@@ -320,7 +320,7 @@ class ApelMysqlDb(object):
             if self.db is not None:
                 self.db.rollback()
             raise
-    
+
     def normalise_summaries(self):
         """
         Normalise data from Summaries and insert into HybridSuperSummaries.
@@ -330,7 +330,7 @@ class ApelMysqlDb(object):
         the results in the HybridSuperSummaries table. This is done by
         calling the NormaliseSummaries stored procedure.
 
-        Any failure will result in the entire transaction being rolled 
+        Any failure will result in the entire transaction being rolled
         back.
         """
         try:
@@ -351,16 +351,16 @@ class ApelMysqlDb(object):
             if self.db is not None:
                 self.db.rollback()
             raise
-    
+
     def copy_summaries(self):
         '''
         Copy summaries from the NormalisedSummaries table to the HybridSuperSummaries table.
-        ''' 
+        '''
         try:
             # prevent MySQLdb from raising
             # 'MySQL server has gone' exception
             self._mysql_reconnect()
-            
+
             c = self.db.cursor()
             log.info("Copying summaries...")
             c.callproc(self._copy_summaries_proc)
@@ -374,27 +374,27 @@ class ApelMysqlDb(object):
             if self.db is not None:
                 self.db.rollback()
             raise
-        
+
     def summarise_cloud(self):
         '''
-        Aggregate the CloudRecords table and put the results in the 
-        CloudSummaries table.  This method does this by calling the 
+        Aggregate the CloudRecords table and put the results in the
+        CloudSummaries table.  This method does this by calling the
         SummariseVMs() stored procedure.
-        
-        Any failure will result in the entire transaction being rolled 
+
+        Any failure will result in the entire transaction being rolled
         back.
         '''
         try:
             # prevent MySQLdb from raising
             # 'MySQL server has gone' exception
             self._mysql_reconnect()
-            
+
             c = self.db.cursor()
-            
+
             log.info("Summarising cloud records...")
             c.callproc(self._summarise_vms_proc, ())
             log.info("Done.")
-            
+
             self.db.commit()
         except MySQLdb.Error, e:
             log.error("A mysql error occurred: %s", e)
@@ -403,12 +403,12 @@ class ApelMysqlDb(object):
             if self.db is not None:
                 self.db.rollback()
             raise
-        
+
     def join_records(self):
         '''
-        This method executes JoinJobRecords procedure in database which joins data 
+        This method executes JoinJobRecords procedure in database which joins data
         from BlahdRecords table and EventRecords table into JobRecord.
-        
+
         For exact implementantion please read the code of JoinJobRecords procedure
         from client.sql in apeldb/schema folder.
         '''
@@ -416,7 +416,7 @@ class ApelMysqlDb(object):
             # prevent MySQLdb from raising
             # 'MySQL server has gone' exception
             self._mysql_reconnect()
-            
+
             c = self.db.cursor()
             log.info("Creating JobRecords")
             c.callproc(self._join_records_proc)
@@ -429,15 +429,15 @@ class ApelMysqlDb(object):
             if self.db is not None:
                 self.db.rollback()
             raise e
-        
+
     def create_local_jobs(self):
         try:
             # prevent MySQLdb from raising
             # 'MySQL server has gone' exception
             self._mysql_reconnect()
-            
+
             c = self.db.cursor()
-            
+
             log.info("Creating local jobs")
             c.callproc(self._local_jobs_proc)
             log.info("Done.")
@@ -448,54 +448,54 @@ class ApelMysqlDb(object):
 
             if self.db is not None:
                 self.db.rollback()
-                
+
             raise e
-    
+
     def update_spec(self, site, ce, spec_level_type, spec_level):
         '''
         This method compares the existing data from database to given values
         and updates the SpecRecords table if it is neccessary.
         '''
-        
+
         try:
-            # prevent MySQLdb from raising 
+            # prevent MySQLdb from raising
             # 'MySQL server has gone' exception
             self._mysql_reconnect()
-            
+
             c = self.db.cursor()
-            
-            c.execute("SELECT %s" % self._spec_lookup_proc, 
-                      [site, ce, spec_level_type, 
+
+            c.execute("SELECT %s" % self._spec_lookup_proc,
+                      [site, ce, spec_level_type,
                        datetime.datetime.now()])
-            
+
             # MySQL 5.1.xx (or MySQL-python 1.2.1) returns None
             # directly from c.fetchone(), so we must handle it in another way
             try:
                 old_level = c.fetchone()[0]
             except TypeError:
                 old_level = None
-                
+
             # Precision limited to third decimal place is enough for our purposes.
-            
+
             if old_level is None:
                 c.execute(self._spec_update_proc,
                            [site, ce, spec_level_type,
                             datetime.datetime.fromtimestamp(0),
                             spec_level])
             elif abs((old_level-decimal.Decimal(str(spec_level)))) > decimal.Decimal('0.001'):
-                c.execute(self._spec_update_proc, 
-                           [site, ce, spec_level_type, 
+                c.execute(self._spec_update_proc,
+                           [site, ce, spec_level_type,
                             datetime.datetime.now(), spec_level])
 
             self.db.commit()
-            
+
         except MySQLdb.Error, e:
             log.error("A mysql error occurred: %s", e)
             log.error("Any transaction will be rolled back.")
-            
+
             if self.db is not None:
                 self.db.rollback()
-        
+
     def clean_processed_files(self, hostname):
         '''
         Cleans ProcessedFiles table from records for given host.
@@ -504,7 +504,7 @@ class ApelMysqlDb(object):
             # prevent MySQLdb from raising
             # 'MySQL server has gone' exception
             self._mysql_reconnect()
-            
+
             c = self.db.cursor()
             c.execute(self._processed_clean, [hostname])
             self.db.commit()
@@ -513,7 +513,7 @@ class ApelMysqlDb(object):
             log.error("Any transaction will be rolled back.")
             if self.db is not None:
                 self.db.rollback()
-    
+
     def _mysql_reconnect(self):
         '''
         Checks if current connection is active and
@@ -523,8 +523,8 @@ class ApelMysqlDb(object):
             self.db.ping()
         except MySQLdb.Error:
             try:
-                self.db = MySQLdb.connect(host=self._db_host, port=self._db_port, 
-                                          user=self._db_username, passwd=self._db_pwd, 
+                self.db = MySQLdb.connect(host=self._db_host, port=self._db_port,
+                                          user=self._db_username, passwd=self._db_pwd,
                                           db=self._db_name)
             except MySQLdb.Error, e:
                 log.error('Error connecting to database: %s', e)
