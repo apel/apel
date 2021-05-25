e59cf79897c218e1ca59655c363a25e04533d48d
Adrian Coveney <adrian.coveney@stfc.ac.uk>
2021-05-25 15:42:52 +0100

    Tidy up unloader_utils tests

--- test/test_unloader_utils.py
+++ test/test_unloader_utils.py
@@ -1,9 +1,5 @@
 import unittest
-import os
-import sys
 import tempfile
-
-_python_version = sys.version_info[0]
 
 try:
     import ConfigParser
@@ -28,15 +24,13 @@ class UnloaderUtilsTest(unittest.TestCase):
         self.resp_upper = utils.RECORDS_PER_MESSAGE_MAX
         self.resp_default = utils.RECORDS_PER_MESSAGE_DEFAULT
 
-
         self.valid_header = 'unloader'
-        self.valid_key = 'records_per_message' 
+        self.valid_key = 'records_per_message'
         self.valid_value = 3456
 
         self.invalid_header = 'notunloader'
-        self.invalid_key = 'notrecords_per_message' 
+        self.invalid_key = 'notrecords_per_message'
         self.invalid_value = None
-
 
     def test_read_config(self):
         """Check that ConfigParser can read the true config file"""
@@ -45,17 +39,17 @@ class UnloaderUtilsTest(unittest.TestCase):
         cp = ConfigParser.ConfigParser()
         cp.read(self.client_config)
 
-        self.assertEqual(self.resp_default, check(cp)) # TODO assumes default matches cfg
-
+        self.assertEqual(self.resp_default, check(cp))
 
     def mkconfig(self, value, header='unloader', key='records_per_message'):
-        try:
-            cfgstr = bytes('[%s]\n%s = %s' % (header, key, value), 'utf-8') # python3
-        except:
-            cfgstr = bytes('[%s]\n%s = %s' % (header, key, value)) # python2.7
+        try:  # Python 3
+            cfgstr = bytes('[%s]\n%s = %s' % (header, key, value), 'utf-8')
+        except:  # Python 2.7
+            cfgstr = bytes('[%s]\n%s = %s' % (header, key, value))
         tmp = tempfile.NamedTemporaryFile()
         tmp.write(cfgstr)
-        tmp.seek(0) # Avoid closing temporary file, which deletes it immediately.
+        # Avoid closing temporary file, which deletes it immediately.
+        tmp.seek(0)
         return tmp
 
     def _assertCase(self, header, key, value, exp):
@@ -72,9 +66,8 @@ class UnloaderUtilsTest(unittest.TestCase):
     def test_check_records_per_message_valid(self):
         """Check helper function output for valid config"""
 
-        file = 'valid'
-        header = self.valid_header 
-        key = self.valid_key 
+        header = self.valid_header
+        key = self.valid_key
         value = self.valid_value
         exp = self.valid_value
 
@@ -83,9 +76,8 @@ class UnloaderUtilsTest(unittest.TestCase):
     def test_check_records_per_message_low_value(self):
         """Check helper function output for too low config value"""
 
-        file = 'low_value'
-        header = self.valid_header 
-        key = self.valid_key 
+        header = self.valid_header
+        key = self.valid_key
         value = self.resp_lower - 1
         exp = self.resp_lower
 
@@ -94,9 +86,8 @@ class UnloaderUtilsTest(unittest.TestCase):
     def test_check_records_per_message_high_value(self):
         """Check helper function output for too high config value"""
 
-        file = 'high_value'
-        header = self.valid_header 
-        key = self.valid_key 
+        header = self.valid_header
+        key = self.valid_key
         value = self.resp_upper + 1
         exp = self.resp_upper
 
@@ -105,9 +96,8 @@ class UnloaderUtilsTest(unittest.TestCase):
     def test_check_records_per_message_invalid_value(self):
         """Check helper function output for invalid config value"""
 
-        file = 'invalid_value'
-        header = self.valid_header 
-        key = self.valid_key 
+        header = self.valid_header
+        key = self.valid_key
         value = self.invalid_value
         exp = self.resp_default
 
@@ -116,9 +106,8 @@ class UnloaderUtilsTest(unittest.TestCase):
     def test_check_records_per_message_invalid_header(self):
         """Check helper function output for missing config header"""
 
-        file = 'invalid_header'
-        header = self.invalid_header 
-        key = self.valid_key 
+        header = self.invalid_header
+        key = self.valid_key
         value = self.valid_value
         exp = self.resp_default
 
@@ -127,14 +116,13 @@ class UnloaderUtilsTest(unittest.TestCase):
     def test_check_records_per_message_invalid_key(self):
         """Check helper function output for invalid config key"""
 
-        file = 'invalid_key'
-        header = self.valid_header 
-        key = self.invalid_key 
+        header = self.valid_header
+        key = self.invalid_key
         value = self.valid_value
         exp = self.resp_default
 
         self._assertCase(header, key, value, exp)
-        
+
 
 if __name__ == '__main__':
     unittest.main()
