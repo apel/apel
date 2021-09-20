9b934505d5eccc4487b2cff87178065c104404fa
tvv28669 <connor.pettitt@stfc.ac.uk>
2021-09-20 13:32:43 +0100

    ALTER TABLE to remove CloudType from primary key

--- scripts/update-x.x.x-x.x.x.sql
+++ scripts/update-x.x.x-x.x.x.sql
@@ -14,48 +14,13 @@
 --   ensures new summaries overwrite old summaries after updates
 --   to CASO version name.
 
-DROP TABLE IF EXISTS CloudSummaries;
-CREATE TABLE CloudSummaries (
-  UpdateTime TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-
-  SiteID INT NOT NULL, -- Foreign key
-  CloudComputeServiceID INT NOT NULL, -- Foreign key
-
-  Month INT NOT NULL,
-  Year INT NOT NULL,
-
-  GlobalUserNameID INT NOT NULL, -- Foreign key
-  VOID INT NOT NULL, -- Foreign key
-  VOGroupID INT NOT NULL, -- Foreign key
-  VORoleID INT NOT NULL, -- Foreign key
-
-  Status VARCHAR(255) NOT NULL,
-  CloudType VARCHAR(255) NOT NULL,
-  ImageId VARCHAR(255) NOT NULL,
-
-  EarliestStartTime DATETIME,
-  LatestStartTime DATETIME,
-  WallDuration BIGINT,
-  CpuDuration BIGINT,
-  CpuCount INT NOT NULL,
-
-  NetworkInbound BIGINT,
-  NetworkOutbound BIGINT,
-  -- PublicIPCount BIGINT,
-  Memory BIGINT,
-  Disk BIGINT,
-
-  BenchmarkType VARCHAR(50) NOT NULL,
-  Benchmark DECIMAL(10,3) NOT NULL,
-
-  NumberOfVMs INT,
-
-  PublisherDNID VARCHAR(255),
-
-  PRIMARY KEY (SiteID, CloudComputeServiceID, Month, Year, GlobalUserNameID,
+ALTER TABLE CloudSummaries 
+  DROP PRIMARY KEY,
+  ADD PRIMARY KEY (
+    SiteID, CloudComputeServiceID, Month, Year, GlobalUserNameID,
     VOID, VOGroupID, VORoleID, Status, ImageId, CpuCount,
-    BenchmarkType, Benchmark)
-
-);
+    BenchmarkType, Benchmark
+  );
 
 */
+
