32638f2f22bb50ff9380a69ff8b733b8286e510b
tvv28669 <connor.pettitt@stfc.ac.uk>
2021-10-07 13:50:15 +0100

    Avoid separating CloudSummaries by irrelevant CloudType values

--- schemas/cloud.sql
+++ schemas/cloud.sql
@@ -284,7 +284,7 @@ BEGIN
       'summariser'
       FROM TVMUsagePerMonth
       GROUP BY SiteID, CloudComputeServiceID, Month, Year, GlobalUserNameID, VOID,
-          VOGroupID, VORoleID, Status, CloudType, ImageId, CpuCount,
+          VOGroupID, VORoleID, Status, ImageId, CpuCount,
           BenchmarkType, Benchmark
       ORDER BY NULL;
 END //
