922d8a9d0d6939c033838d3efc1c423b80ebe80b
Ian Neilson <ian.neilson@stfc.ac.uk>
2022-01-17 11:49:28 +0000

    Add test to check query

--- test/test_unloader.py
+++ test/test_unloader.py
@@ -3,6 +3,8 @@ import shutil
 import tempfile
 import unittest
 
+from mock import mock
+from apel.db.backends.mysql import ApelMysqlDb
 from apel.db.apeldb import ApelDbException
 from apel.db.records import StorageRecord
 from apel.db.unloader import DbUnloader, get_start_of_previous_month
@@ -16,7 +18,15 @@ class TestDbUnloader(unittest.TestCase):
     def setUp(self):
         # Create temporary directory for dirq to write to
         self.dir_path = tempfile.mkdtemp()
-        self.unloader = DbUnloader(None, self.dir_path)
+
+        # Mock out the database as we're not testing that here
+        # Note that as it's a class it needs instantiating
+        self.mock_db = mock.patch(__name__ + '.ApelMysqlDb', autospec=True, spec_set=True)
+        self.mock_db.start()
+
+        self._apeldb = ApelMysqlDb('host', 1234, 'user', 'pwd', 'db')
+
+        self.unloader = DbUnloader(self._apeldb, self.dir_path)
 
     def tearDown(self):
         shutil.rmtree(self.dir_path)
@@ -25,6 +35,30 @@ class TestDbUnloader(unittest.TestCase):
         """Check that unloading StAR records to APEL format is prevented."""
         self.assertRaises(ApelDbException, self.unloader._write_messages,
                           StorageRecord, 'table', 'query', ur=False)
+
+    def test_gap_unload_vstar_fails(self):
+        """Check that trying to gap unload unsupported record type fails"""
+        dtStart = '2011-01-01'
+        dtEnd = '2011-12-01'
+
+        self.assertRaises(ApelDbException, self.unloader.unload_gap, 'VStarRecords', dtStart, dtEnd, ur=False)
+
+    def test_gap_unload_summaries(self):
+        """Check summary gap unload query"""
+        dtStart = '2011-01-01'
+        dtEnd = '2011-12-01'
+
+        msgs, records = self.unloader.unload_gap('VSummaries', dtStart, dtEnd, ur=False)
+
+        self._apeldb.get_records.assert_called_once()
+
+        callArgs, callKWArgs = self._apeldb.get_records.call_args
+
+        self.assertEqual('VSummaries', callArgs[1])
+
+        expected = " WHERE EarliestEndTime > '2011-01-01 00:00:00' AND LatestEndTime <= '2011-12-02 00:00:00' AND InfrastructureType = 'grid'"
+
+        self.assertEqual(expected, callKWArgs['query'].get_where())
 
 
 class TestFunctions(unittest.TestCase):
