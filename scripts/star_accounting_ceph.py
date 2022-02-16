8079ce9b30e15539c0d38e133ec31f1db9c809b0
jounaidr <52418954+jounaidr@users.noreply.github.com>
2022-02-16 18:38:38 +0000

    Update star_accounting_ceph.py

--- scripts/star_accounting_ceph.py
+++ scripts/star_accounting_ceph.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-#   Copyright (C) 2012 STFC
+#   Copyright (C) 2022 STFC
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
