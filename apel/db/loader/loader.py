6002d893b1985ed6148ad52e2960076b14201030
Sae126V <saitejav2021@gmail.com>
2023-06-28 15:09:45 +0000

    [GT-169] Update code to fix indentation

--- apel/db/loader/loader.py
+++ apel/db/loader/loader.py
@@ -147,12 +147,10 @@ class Loader(object):
             try:
                 log.info("Loading message %s. ID = %s", self.current_msg, msg_id)
 
-                if (
-                    not data or
-                    not data['body'].strip() or
-                    not data['signer'].strip() or
-                    not data['empaid'].strip()
-                ):
+                if (not data or
+                        not data['body'].strip() or
+                        not data['signer'].strip() or
+                        not data['empaid'].strip()):
                     raise LoaderException(
                         "Cannot load incomplete message from an incoming queue"
                     )
