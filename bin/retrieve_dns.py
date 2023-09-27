d90152b3b7d57b83a9d80751d0d00d02c6e4466e
RedProkofiev <nicholaswhyatt@gmail.com>
2023-09-27 09:34:19 +0000

    testing changes

--- bin/retrieve_dns.py
+++ bin/retrieve_dns.py
@@ -157,6 +157,7 @@ def dns_from_dom(dom):
                 # We haven't accepted the DN, so write it to the log file.
                 log.warning("DN not valid and won't be added: %s", dn)
 
+    print(dns)
     return dns
 
 def dns_from_file(path):
@@ -299,6 +300,7 @@ def runprocess(config_file, log_config_file):
 
     # print all the the dns to a file, with the discarded ones to a second file
     try:
+        print("Writing")
         new_dn_file = open(cfg.dn_file, 'w')
     except IOError, e:
         log.warning("Failed to open file %s for writing.", cfg.dn_file)
