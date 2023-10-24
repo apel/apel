a5232c8058f464e4a2d7d563bcae5eb73d4abd99
Adrian Coveney <adrian.coveney@stfc.ac.uk>
2023-10-24 11:27:06 +0100

    Future proof dbunloader code
    - Fix except...as
    - Make ConfigParser import handle future lowercase name.
    - Tidy string formatting

--- bin/dbunloader.py
+++ bin/dbunloader.py
@@ -25,7 +25,10 @@ from apel.db import ApelDb, ApelDbException
 from apel.db.unloader import DbUnloader
 from apel import __version__
 from optparse import OptionParser
-import ConfigParser
+try:
+    import ConfigParser
+except ImportError:
+    import configparser as ConfigParser
 
 
 RECORDS_PER_MESSAGE_MIN = 1
@@ -57,9 +60,9 @@ if __name__ == '__main__':
                            cp.get('logging', 'level'),
                            cp.getboolean('logging', 'console'))
         log = logging.getLogger('dbunloader')
-    except (ConfigParser.Error, ValueError, IOError), err:
-        print 'Error configuring logging: %s' % str(err)
-        print 'The system will exit.'
+    except (ConfigParser.Error, ValueError, IOError) as err:
+        print('Error configuring logging: %s' % err)
+        print('The system will exit.')
         sys.exit(1)
 
     db = None
@@ -75,10 +78,10 @@ if __name__ == '__main__':
                     dbcp.get('db', 'password'),
                     dbcp.get('db', 'name'))
 
-    except ApelDbException, e:
+    except ApelDbException as e:
         log.fatal('Error: %s', e)
         sys.exit(1)
-    except Exception, e:
+    except Exception as e:
         log.fatal('Cannot get configuration: %s', e)
         sys.exit(1)
 
@@ -166,7 +169,7 @@ if __name__ == '__main__':
         log.info('%d records in %d messages unloaded from %s', recs, msgs, table_name)
     except KeyError:
         log.error('Invalid table name: %s, omitting', table_name)
-    except ApelDbException, e:
+    except ApelDbException as e:
         log.error('Unloading failed: %s', e)
 
     log.info('Unloading complete.')
