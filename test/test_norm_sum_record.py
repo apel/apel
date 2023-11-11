daeecdf52d188659163171cd2a20419b1b4c2f7e
Adrian Coveney <adrian.coveney@stfc.ac.uk>
2023-11-11 16:33:16 +0000

    Add basic Norm Sum 04 test by refactor of existing

--- test/test_norm_sum_record.py
+++ test/test_norm_sum_record.py
@@ -19,7 +19,8 @@ from future.builtins import str, zip
 from datetime import datetime, timedelta
 import unittest
 
-from apel.db.records import NormalisedSummaryRecord, InvalidRecordException
+from apel.db.records import (NormalisedSummaryRecord, NormalisedSummaryRecord04,
+                             InvalidRecordException)
 
 
 class TestNormalisedSummaryRecord(unittest.TestCase):
@@ -27,22 +28,25 @@ class TestNormalisedSummaryRecord(unittest.TestCase):
     Tests the NormalisedSummaryRecord class.
     """
 
+    RECORD_TYPE = NormalisedSummaryRecord
+
     def setUp(self):
         """Run before each test."""
         self._records = self._get_record_text()
         self._tuples = self._get_record_tuples()
         self._wrong_records = self._get_wrong_records()
+        self.record_type = self.RECORD_TYPE
 
     def test_load_from_msg(self):
         """This currently checks that an exception is not thrown."""
         for record in self._records:
-            nsr = NormalisedSummaryRecord()
+            nsr = self.record_type()
             nsr.load_from_msg(record)
 
     def test_get_msg(self):
         """This currently checks that an exception is not thrown."""
         for record in self._records:
-            nsr = NormalisedSummaryRecord()
+            nsr = self.record_type()
             nsr.load_from_msg(record)
 
             # not a proper test yet
@@ -51,7 +55,7 @@ class TestNormalisedSummaryRecord(unittest.TestCase):
     def test_load_from_msg_wrong(self):
         """Check that invalid records don't get loaded."""
 
-        nsr = NormalisedSummaryRecord()
+        nsr = self.record_type()
 
         # N.B. When values are being loaded from messages, they are all strings.
         # Not a dictionary because I want to use more than one value per key.
@@ -69,7 +73,7 @@ class TestNormalisedSummaryRecord(unittest.TestCase):
         """This checks that an exception is thrown when invalid
         records are loaded."""
         for record in self._wrong_records:
-            nsr = NormalisedSummaryRecord()
+            nsr = self.record_type()
 
             self.assertRaises(InvalidRecordException, nsr.load_from_msg, record)
 
@@ -79,7 +83,7 @@ class TestNormalisedSummaryRecord(unittest.TestCase):
 
         for rec_txt, rec_tuple in zip(self._records, self._tuples):
 
-            nsr = NormalisedSummaryRecord()
+            nsr = self.record_type()
             nsr.load_from_msg(rec_txt)
 
             test_dn = "This is a test DN."
@@ -98,7 +102,7 @@ class TestNormalisedSummaryRecord(unittest.TestCase):
 
     def test_get_ur(self):
         """Check that get_ur outputs correct XML."""
-        nsr = NormalisedSummaryRecord()
+        nsr = self.record_type()
         nsr.load_from_msg(self._records[0])
         xml = ('<aur:SummaryRecord><aur:Site>RAL-LCG2</aur:Site><aur:Month>3</a'
                'ur:Month><aur:Year>2010</aur:Year><aur:UserIdentity><urf:GroupA'
@@ -256,6 +260,160 @@ class TestNormalisedSummaryRecord(unittest.TestCase):
             CpuDuration: 2345
             NormalisedWallDuration: 234256
             NormalisedCpuDuration: 244435
+            NumberOfJobs: 100"""
+
+        records.append(record_text)
+        records.append(record_text2)
+        records.append(record_text3)
+        return records
+
+
+class TestNormalisedSummaryRecord04(TestNormalisedSummaryRecord):
+    """Test for the NormalisedSummaryRecord04 class."""
+
+    RECORD_TYPE = NormalisedSummaryRecord04
+
+    def _get_record_text(self):
+        """Gets some sample valid records, as a tuple of strings.  The contents
+        must match the values in _get_record_tuples."""
+
+        records = []
+        record_text = """Site: RAL-LCG2
+            Month: 3
+            Year: 2010
+            GlobalUserName: /C=whatever/D=someDN
+            VO: atlas
+            VOGroup: /atlas
+            VORole: Role=production
+            SubmitHost: some.host.org
+            Infrastructure: grid
+            EarliestEndTime: 1267405200
+            LatestEndTime: 1269046800
+            WallDuration: 234256
+            CpuDuration: 244435
+            NormalisedWallDuration: {HEPscore23: 234256}
+            NormalisedCpuDuration: {HEPscore23: 244435}
+            NumberOfJobs: 100"""
+
+        record_text2 = """Site: RAL-LCG2
+            Month: 4
+            Year: 2010
+            GlobalUserName: /C=whatever/D=someDN
+            VO: atlas
+            VOGroup: /atlas
+            VORole: Role=production
+            SubmitHost: some.host.org
+            Infrastructure: local
+            EarliestEndTime: 1270083600
+            LatestEndTime: 1271725200
+            WallDuration: 234256
+            CpuDuration: 244435
+            NormalisedWallDuration: {hepspec: 234256}
+            NormalisedCpuDuration: {hepspec: 244435}
+            NumberOfJobs: 100"""
+
+        record_text3 = """Site: RAL-LCG2
+            Month: 12
+            Year: 2010
+            GlobalUserName: /C=whatever/D=someDN
+            VO: atlas
+            VOGroup: /atlas
+            VORole: Role=production
+            SubmitHost: some.host.org
+            Infrastructure: local
+            EarliestEndTime: 1291161600
+            LatestEndTime: 1291162100
+            Processors: 1
+            NodeCount: 1
+            WallDuration: 234256
+            CpuDuration: 244435
+            NormalisedWallDuration: {si2k: 234256}
+            NormalisedCpuDuration: {si2k: 244435}
+            NumberOfJobs: 100"""
+
+        records.append(record_text)
+        records.append(record_text2)
+        records.append(record_text3)
+        return records
+
+    def _get_record_tuples(self):
+        """
+        Get tuples with contents in the correct order to go into the
+        stored procedure with the same values as in the messages in
+        _get_record_text().
+        """
+        tuples = []
+
+        tuple1 = ('RAL-LCG2', 3, 2010, '/C=whatever/D=someDN', 'atlas',
+                  '/atlas',   'Role=production', 'some.host.org', 'grid', 'hepscore23',
+                  None, None, datetime.utcfromtimestamp(1267405200),
+                  datetime.utcfromtimestamp(1269046800), 234256, 244435, 234256,
+                  244435, 100)
+
+        tuple2 = ('RAL-LCG2', 4, 2010, '/C=whatever/D=someDN', 'atlas',
+                  '/atlas', 'Role=production', 'some.host.org', 'local', 'hepspec',
+                  None, None, datetime.utcfromtimestamp(1270083600),
+                  datetime.utcfromtimestamp(1271725200), 234256, 244435, 234256,
+                  244435, 100)
+
+        tuple3 = ('RAL-LCG2', 12, 2010, '/C=whatever/D=someDN', 'atlas',
+                  '/atlas', 'Role=production', 'some.host.org', 'local', 'si2k', 1, 1,
+                  datetime.utcfromtimestamp(1291161600),
+                  datetime.utcfromtimestamp(1291162100), 234256, 244435, 234256,
+                  244435, 100)
+
+        tuples.append(tuple1)
+        tuples.append(tuple2)
+        tuples.append(tuple3)
+
+        return tuples
+
+    def _get_wrong_records(self):
+
+        records = []
+
+        # wrong month
+        record_text = """Site: RAL-LCG2
+            Month: 13
+            Year: 2010
+            GlobalUserName: /C=whatever/D=someDN
+            VO: atlas
+            VOGroup: /atlas
+            VORole: Role=production
+            WallDuration: 234256
+            CpuDuration: 2345
+            NormalisedWallDuration: {HEPscore23: 234256}
+            NormalisedCpuDuration: {HEPscore23: 244435}
+            NumberOfJobs: 100"""
+
+        # negative number
+        record_text2 = """Site: RAL-LCG2
+            Month: 4
+            Year: 2010
+            GlobalUserName: /C=whatever/D=someDN
+            VO: atlas
+            VOGroup: /atlas
+            VORole: Role=production
+            WallDuration: -234256
+            CpuDuration: 2345
+            NormalisedWallDuration: {hepspec: 234256}
+            NormalisedCpuDuration: {hepspec: 244435}
+            NumberOfJobs: 100"""
+
+        # times outside of month
+        record_text3 = """Site: RAL-LCG2
+            Month: 5
+            Year: 2010
+            GlobalUserName: /C=whatever/D=someDN
+            VO: atlas
+            VOGroup: /atlas
+            VORole: Role=production
+            EarliestEndTime: 1234567890
+            LatestEndTime: 1310641063
+            WallDuration: 234256
+            CpuDuration: 2345
+            NormalisedWallDuration: {si2k: 234256}
+            NormalisedCpuDuration: {si2k: 244435}
             NumberOfJobs: 100"""
 
         records.append(record_text)
